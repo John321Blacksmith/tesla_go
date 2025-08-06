8b5ebb5665922d13f67816b71b2f3a41abcebbe1
Ivan Kuzmichev <ivan.kuzmichev@red-soft.ru>
2025-08-06 15:15:02 +0300

    feat: entrypint

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,25 @@
 package main
 
-func main() {
-	// input := `Little they know, the ice cream was invented in Russia
-	// 		  This kind of food was liked by almost everyone in the world
-	// 		  because of it's taste and unique nutrient features`
+import (
+	"fmt"
+	"tesla_go/internal/usecase"
+)
 
+func main() {
+	input := `Little they know, the ice cream was invented in Russia.
+			  This kind of food was liked by almost everyone in the world.
+			  because of it's taste. and unique nutrient features`
+
+	inputProcessing := usecase.NewInputUseCase()
+	rawSentences := inputProcessing.SplitToParticles(input, ".")
+	fmt.Println(len(rawSentences))
+	refinedSentences := inputProcessing.RefineSentences(rawSentences)
+	fmt.Println(len(refinedSentences))
+	sentenceCollection, err := inputProcessing.AggregateSentences(refinedSentences)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for i := range len(sentenceCollection) {
+		fmt.Println(sentenceCollection[i])
+	}
 }
