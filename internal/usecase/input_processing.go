5b34f8e62c303cbf4e876665c8296e0ceb6ea967
John321Blacksmith <kuzmichevi321@gmail.com>
2025-08-08 12:56:16 +0300

    feat: applied interface for the usecase

--- internal/usecase/input_processing.go
+++ internal/usecase/input_processing.go
@@ -2,32 +2,32 @@ package usecase
 
 import (
 	"tesla_go/internal/domain"
-	"tesla_go/pkg/classifier"
 )
 
 type InputManagement interface {
-	ProcessInput(data []byte) string
 	SplitToParticles(data string, sym string) []string
 	RefineSentences(rawSentences []string) []string
-	AggregateSentences(refinedSentences []string) []domain.Sentence
+	AggregateSentences(refinedSentences []string) ([]domain.Sentence, error)
 }
 
 type InputUseCase struct {
-	InputManagement
+	behaviour InputManagement
 }
 
-func NewInputUseCase() InputUseCase {
-	return InputUseCase{}
+func NewInputUseCase(behaviour InputManagement) InputUseCase {
+	return InputUseCase{
+		behaviour: behaviour,
+	}
 }
 
 func (uc *InputUseCase) SplitToParticles(data string, sym string) []string {
-	return classifier.SplitToParticles(data, sym)
+	return uc.behaviour.SplitToParticles(data, sym)
 }
 
 func (uc *InputUseCase) RefineSentences(rawSentences []string) []string {
-	return classifier.RefineSentences(rawSentences)
+	return uc.behaviour.RefineSentences(rawSentences)
 }
 
 func (uc *InputUseCase) AggregateSentences(refinedSentences []string) ([]domain.Sentence, error) {
-	return classifier.AggregateSentences(refinedSentences)
+	return uc.behaviour.AggregateSentences(refinedSentences)
 }
